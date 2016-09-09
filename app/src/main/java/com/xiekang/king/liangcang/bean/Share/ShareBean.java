1aa691ed383a44d9bbdc12b74c8b59be00bdc3fe
supercaorui <879335066@qq.com>
2016-09-09 16:04:58 +0800

    share功能的实现 2016/9.9

---
+++ app/src/main/java/com/xiekang/king/liangcang/bean/Share/ShareBean.java
@@ -0,0 +1,290 @@
+package com.xiekang.king.liangcang.bean.Share;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/9/7.
+ */
+public class ShareBean {
+    /**
+     * status : 0
+     * server_time : 2016-09-08 02:21:41
+     * account_id : 0
+     * cost : 0.125657
+     * errdata : null
+     * errmsg :
+     */
+
+    private MetaBean meta;
+    /**
+     * meta : {"status":0,"server_time":"2016-09-08 02:21:41","account_id":0,"cost":0.125657,"errdata":null,"errmsg":""}
+     * version : 1
+     * data : {"has_more":true,"num_items":"13904","items":[{"goods_id":"254496","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254496.jpg","goods_name":"Gourmet Traveller","price":"0","owner_id":"1000018759","comment_count":"2","like_count":"2","liked":"0","sale_by":"other","goods_url":"http://www.gourmettraveller.com.au","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254495","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254495.jpg","goods_name":"Casa Brutus","price":"0","owner_id":"1000018759","comment_count":"0","like_count":"2","liked":"0","sale_by":"other","goods_url":"http://magazineworld.jp/casabrutus/books/","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254494","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254494.jpg","goods_name":"HOCHPARTERRE","price":"0","owner_id":"1000018759","comment_count":"0","like_count":"0","liked":"0","sale_by":"other","goods_url":"http://www.hochparterre.ch","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254491","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254491.jpg","goods_name":"ZEIT MAGAZIN","price":"0","owner_id":"1000018759","comment_count":"0","like_count":"2","liked":"0","sale_by":"other","goods_url":"http://www.zeit.de/zeit-magazin/index","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254490","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254490.jpg","goods_name":"AD Spain AD Germerny","price":"0","owner_id":"1000018759","comment_count":"0","like_count":"1","liked":"0","sale_by":"other","goods_url":"https://www.amazon.com/ref=nav_logo","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254489","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254489.jpg","goods_name":"art 4d","price":"0","owner_id":"1000018759","comment_count":"1","like_count":"0","liked":"0","sale_by":"other","goods_url":"http://art4d.com/","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"253162","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/253/253162.jpg","goods_name":"Face","price":"0","owner_id":"999999673","comment_count":"17","like_count":"18","liked":"0","sale_by":"other","goods_url":"http://www.dewilewis.com/products/face","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"253161","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/253/253161.jpg","goods_name":"Until Death Do Us Part","price":"0","owner_id":"999999673","comment_count":"4","like_count":"11","liked":"0","sale_by":"other","goods_url":"http://www.amazon.com/Until-Death-Do-Us-Part/dp/B00ZIJN690/ref=pd_sim_14_1?ie=UTF8&dpID=51Nv4OvJi9L&dpSrc=sims&preST=_AC_UL160_SR105%2C160_&refRID=0VRMSWSCM108YD3VDB6H","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"253160","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/253/253160.jpg","goods_name":"Songbook","price":"0","owner_id":"999999673","comment_count":"8","like_count":"5","liked":"0","sale_by":"other","goods_url":"http://www.amazon.com/Songbook-Alec-Soth/dp/191016402X/ref=pd_sim_14_2?ie=UTF8&dpID=61x8K-Ht24L&dpSrc=sims&preST=_AC_UL160_SR160%2C160_&refRID=0VRMSWSCM108YD3VDB6H","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"253159","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/253/253159.jpg","goods_name":"Cabanagem","price":"0","owner_id":"999999673","comment_count":"2","like_count":"7","liked":"0","sale_by":"other","goods_url":"http://www.editionsbessard.com/product/cabanagem-andre-penteado-editora-madalena/","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"}]}
+     */
+
+    private int version;
+    /**
+     * has_more : true
+     * num_items : 13904
+     * items : [{"goods_id":"254496","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254496.jpg","goods_name":"Gourmet Traveller","price":"0","owner_id":"1000018759","comment_count":"2","like_count":"2","liked":"0","sale_by":"other","goods_url":"http://www.gourmettraveller.com.au","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254495","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254495.jpg","goods_name":"Casa Brutus","price":"0","owner_id":"1000018759","comment_count":"0","like_count":"2","liked":"0","sale_by":"other","goods_url":"http://magazineworld.jp/casabrutus/books/","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254494","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254494.jpg","goods_name":"HOCHPARTERRE","price":"0","owner_id":"1000018759","comment_count":"0","like_count":"0","liked":"0","sale_by":"other","goods_url":"http://www.hochparterre.ch","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254491","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254491.jpg","goods_name":"ZEIT MAGAZIN","price":"0","owner_id":"1000018759","comment_count":"0","like_count":"2","liked":"0","sale_by":"other","goods_url":"http://www.zeit.de/zeit-magazin/index","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254490","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254490.jpg","goods_name":"AD Spain AD Germerny","price":"0","owner_id":"1000018759","comment_count":"0","like_count":"1","liked":"0","sale_by":"other","goods_url":"https://www.amazon.com/ref=nav_logo","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"254489","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254489.jpg","goods_name":"art 4d","price":"0","owner_id":"1000018759","comment_count":"1","like_count":"0","liked":"0","sale_by":"other","goods_url":"http://art4d.com/","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"253162","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/253/253162.jpg","goods_name":"Face","price":"0","owner_id":"999999673","comment_count":"17","like_count":"18","liked":"0","sale_by":"other","goods_url":"http://www.dewilewis.com/products/face","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"253161","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/253/253161.jpg","goods_name":"Until Death Do Us Part","price":"0","owner_id":"999999673","comment_count":"4","like_count":"11","liked":"0","sale_by":"other","goods_url":"http://www.amazon.com/Until-Death-Do-Us-Part/dp/B00ZIJN690/ref=pd_sim_14_1?ie=UTF8&dpID=51Nv4OvJi9L&dpSrc=sims&preST=_AC_UL160_SR105%2C160_&refRID=0VRMSWSCM108YD3VDB6H","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"253160","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/253/253160.jpg","goods_name":"Songbook","price":"0","owner_id":"999999673","comment_count":"8","like_count":"5","liked":"0","sale_by":"other","goods_url":"http://www.amazon.com/Songbook-Alec-Soth/dp/191016402X/ref=pd_sim_14_2?ie=UTF8&dpID=61x8K-Ht24L&dpSrc=sims&preST=_AC_UL160_SR160%2C160_&refRID=0VRMSWSCM108YD3VDB6H","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"},{"goods_id":"253159","goods_image":"http://imgs-qn.iliangcang.com/ware/goods/big/2/253/253159.jpg","goods_name":"Cabanagem","price":"0","owner_id":"999999673","comment_count":"2","like_count":"7","liked":"0","sale_by":"other","goods_url":"http://www.editionsbessard.com/product/cabanagem-andre-penteado-editora-madalena/","is_gift":0,"discount_price":"0","coupon_flag":"False","promotion_imgurl":"null"}]
+     */
+
+    private DataBean data;
+
+    public MetaBean getMeta() {
+        return meta;
+    }
+
+    public void setMeta(MetaBean meta) {
+        this.meta = meta;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public static class MetaBean {
+        private int status;
+        private String server_time;
+        private int account_id;
+        private double cost;
+        private Object errdata;
+        private String errmsg;
+
+        public int getStatus() {
+            return status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+        public String getServer_time() {
+            return server_time;
+        }
+
+        public void setServer_time(String server_time) {
+            this.server_time = server_time;
+        }
+
+        public int getAccount_id() {
+            return account_id;
+        }
+
+        public void setAccount_id(int account_id) {
+            this.account_id = account_id;
+        }
+
+        public double getCost() {
+            return cost;
+        }
+
+        public void setCost(double cost) {
+            this.cost = cost;
+        }
+
+        public Object getErrdata() {
+            return errdata;
+        }
+
+        public void setErrdata(Object errdata) {
+            this.errdata = errdata;
+        }
+
+        public String getErrmsg() {
+            return errmsg;
+        }
+
+        public void setErrmsg(String errmsg) {
+            this.errmsg = errmsg;
+        }
+    }
+
+    public static class DataBean {
+        private boolean has_more;
+        private String num_items;
+        /**
+         * goods_id : 254496
+         * goods_image : http://imgs-qn.iliangcang.com/ware/goods/big/2/254/254496.jpg
+         * goods_name : Gourmet Traveller
+         * price : 0
+         * owner_id : 1000018759
+         * comment_count : 2
+         * like_count : 2
+         * liked : 0
+         * sale_by : other
+         * goods_url : http://www.gourmettraveller.com.au
+         * is_gift : 0
+         * discount_price : 0
+         * coupon_flag : False
+         * promotion_imgurl : null
+         */
+
+        private List<ItemsBean> items;
+
+        public boolean isHas_more() {
+            return has_more;
+        }
+
+        public void setHas_more(boolean has_more) {
+            this.has_more = has_more;
+        }
+
+        public String getNum_items() {
+            return num_items;
+        }
+
+        public void setNum_items(String num_items) {
+            this.num_items = num_items;
+        }
+
+        public List<ItemsBean> getItems() {
+            return items;
+        }
+
+        public void setItems(List<ItemsBean> items) {
+            this.items = items;
+        }
+
+        public static class ItemsBean {
+            private String goods_id;
+            private String goods_image;
+            private String goods_name;
+            private String price;
+            private String owner_id;
+            private String comment_count;
+            private String like_count;
+            private String liked;
+            private String sale_by;
+            private String goods_url;
+            private int is_gift;
+            private String discount_price;
+            private String coupon_flag;
+            private String promotion_imgurl;
+
+            public String getGoods_id() {
+                return goods_id;
+            }
+
+            public void setGoods_id(String goods_id) {
+                this.goods_id = goods_id;
+            }
+
+            public String getGoods_image() {
+                return goods_image;
+            }
+
+            public void setGoods_image(String goods_image) {
+                this.goods_image = goods_image;
+            }
+
+            public String getGoods_name() {
+                return goods_name;
+            }
+
+            public void setGoods_name(String goods_name) {
+                this.goods_name = goods_name;
+            }
+
+            public String getPrice() {
+                return price;
+            }
+
+            public void setPrice(String price) {
+                this.price = price;
+            }
+
+            public String getOwner_id() {
+                return owner_id;
+            }
+
+            public void setOwner_id(String owner_id) {
+                this.owner_id = owner_id;
+            }
+
+            public String getComment_count() {
+                return comment_count;
+            }
+
+            public void setComment_count(String comment_count) {
+                this.comment_count = comment_count;
+            }
+
+            public String getLike_count() {
+                return like_count;
+            }
+
+            public void setLike_count(String like_count) {
+                this.like_count = like_count;
+            }
+
+            public String getLiked() {
+                return liked;
+            }
+
+            public void setLiked(String liked) {
+                this.liked = liked;
+            }
+
+            public String getSale_by() {
+                return sale_by;
+            }
+
+            public void setSale_by(String sale_by) {
+                this.sale_by = sale_by;
+            }
+
+            public String getGoods_url() {
+                return goods_url;
+            }
+
+            public void setGoods_url(String goods_url) {
+                this.goods_url = goods_url;
+            }
+
+            public int getIs_gift() {
+                return is_gift;
+            }
+
+            public void setIs_gift(int is_gift) {
+                this.is_gift = is_gift;
+            }
+
+            public String getDiscount_price() {
+                return discount_price;
+            }
+
+            public void setDiscount_price(String discount_price) {
+                this.discount_price = discount_price;
+            }
+
+            public String getCoupon_flag() {
+                return coupon_flag;
+            }
+
+            public void setCoupon_flag(String coupon_flag) {
+                this.coupon_flag = coupon_flag;
+            }
+
+            public String getPromotion_imgurl() {
+                return promotion_imgurl;
+            }
+
+            public void setPromotion_imgurl(String promotion_imgurl) {
+                this.promotion_imgurl = promotion_imgurl;
+            }
+        }
+    }
+}
