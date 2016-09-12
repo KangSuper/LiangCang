76e1205d3fb35c28b38c025238f2c988c9b9154f
supercaorui <879335066@qq.com>
2016-09-12 20:55:46 +0800

    达人部分，详情图片没显示1

---
+++ app/src/main/java/com/xiekang/king/liangcang/bean/Share/Share_conver.java
@@ -0,0 +1,14 @@
+package com.xiekang.king.liangcang.bean.Share;
+
+/**
+ * 分享封面
+ * Created by Administrator on 2016/9/9.
+ */
+public class Share_conver {
+   public String img;
+   public String id;
+    public Share_conver(String img,String id){
+        this.img = img;
+        this.id= id;
+    }
+}
---
+++ app/src/main/java/com/xiekang/king/liangcang/bean/UserInfo/UserFollow.java
@@ -0,0 +1,11 @@
+package com.xiekang.king.liangcang.bean.UserInfo;
+
+/**
+ * Created by Administrator on 2016/9/12.
+ */
+public class UserFollow {
+    public String img;
+    public String name;
+    public String job;
+    public String id;
+}
